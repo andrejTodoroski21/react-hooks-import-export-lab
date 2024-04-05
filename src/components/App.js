1e8b46875ceb78e3333f9d7887db92b5d1cbbfae
Andrej <andrejtodoroski@yahoo.com>
2024-04-05 11:57:55 -0400

    completed

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,8 @@
 import React from "react";
+import NavBar from "./NavBar";
+import Home from "./Home";
+import About from "./About";
+// import  username, city, image  from "src/data/user.js"
 
 function App() {
   return (
